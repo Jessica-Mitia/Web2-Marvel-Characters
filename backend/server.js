19c93f3a686bc56c9f49c2737f800c61c90ebb70
Mitia Jessica <mitiah.jessica@gmail.com>
2025-09-12 15:38:37 +0300

    install cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import characterRoutes from "./routes/character.route.js";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors({ origin: "http://localhost:5173" })); // <-- CORS ici
 app.use(express.json());
 
 app.use("/characters", characterRoutes);
